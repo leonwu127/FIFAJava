5fda52ea7ad8008115954f0045b2da69d7e33bc8
leonwu127 <leonwu127@gmail.com>
2016-11-06 16:31:57 +0000

    This is a powerful project

---
+++ python/Test/Test.py
@@ -0,0 +1,64 @@
+import fut
+import random
+from time import time
+
+fut = fut.Core('leonwu127@icloud.com','2013Zhuangyuan','weiwei','xbox')
+
+class Snipe:
+    def __init__(self, assetId, maxBid, minPrice, maxPrice):
+        self.assetId = assetId
+        self.maxBid = maxBid
+        self.minPrice = minPrice
+        self.maxPrice = maxPrice
+
+players = []
+players.append(Snipe(13732, 8000, 9300, 9400)) #John Terry
+players.append(Snipe(165580, 2400, 3000, 3100)) #Diego Alves
+
+for player in players:
+        timesToSnipe = random.randint(44,65)
+        i = 0
+        while i<=timesToSnipe:
+                items = fut.searchAuctions('player', assetId=player.assetId, max_buy=player.maxBid)
+                for item in items:
+                    time.sleep(random.randint(1,3))
+        time.sleep(random.randint(4,8)) #here is the time for changing player in search
+
+
+SAssetId=190460 #Eriksen
+SMaxBuyNow=9000 #min value you like to buy 
+
+SellMin=9700
+SellMax=9800
+
+TotalCoins=fut.credits
+i = 0;
+while i<500:
+    #
+    if i%2 == 0:
+        SMaxBuyNow = 9000
+    else:
+        SMaxBuyNow = 9200
+    i = i + 1;
+    print "Searching for: %d, Coins: %d" % (SMaxBuyNow, TotalCoins)
+    if TotalCoins < 1000:
+        break;
+    
+    items = fut.searchAuctions('player',assetId=SAssetId,max_buy=SMaxBuyNow)
+    for item in items:
+        if TotalCoins > item['buyNowPrice']:
+            fut.bid(item['tradeId'], item['buyNowPrice'])
+            print "Found: %d - %s - Maxtime: %d" % (item['tradeId'], item['buyNowPrice'], item['expires'])
+            itemsBought = fut.unassigned()
+            for itemBought in itemsBought:
+                idItem = itemBought['id']
+                if fut.sendToTradepile(-1, idItem):
+                    print "Listing..."
+                    fut.sell(idItem, SellMin, buy_now=SellMax)
+                time.sleep(random.randint(5,8))
+            TotalCoins = fut.credits
+        else:
+            print "No coins %s" % (item['buyNowPrice'])
+    if TotalCoins<5000:
+        time.sleep(600)
+    time.sleep(random.randint(1,3))
---
+++ python/Test/futCore.py
@@ -0,0 +1,5 @@
+import fut
+import random
+from time import time
+
+fut = fut.Core('leonwu127@icloud.com','2013Zhuangyuan','weiwei','xbox','146498')
