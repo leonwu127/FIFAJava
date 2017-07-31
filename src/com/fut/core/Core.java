62ae36bb10b88d79018c99eb58a7fe220fdc696f
leonwu127 <leonwu127@gmail.com>
2017-08-01 00:07:30 +0100

    This commit has finished the method of getRedirectUrl

--- src/com/fut/core/Core.java
+++ src/com/fut/core/Core.java
@@ -3,6 +3,7 @@ import com.fut.hash.Hash;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.http.*;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.GzipDecompressingEntity;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -20,22 +21,20 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.*;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HTTP.*;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.*;
 import org.json.simple.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-
-/**
- * Created by leonwu127 on 01/11/2016.
- */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 
@@ -51,14 +50,13 @@ public class Core {
 
 
 
-    public Core(){
-        email = null;
-        passwd = null;
-        secretAnswer = null;
-        platform = "xbox";
-        code = null;
-        emulate = "webapp";
-        
+    public Core(String email, String passwd, String secretAnswer){
+        this.email = email;
+        this.passwd = passwd;
+        this.secretAnswer = secretAnswer;
+        this.platform = "pc";
+        this.code = null;
+
     }
 
 
@@ -81,85 +79,148 @@ public class Core {
     }
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        //HttpParams params = httpClient.getParams();
-        //HttpClientParams.setRedirecting(params, false);
 
-        HttpGet hg = new HttpGet("https://www.easports.com/uk/fifa/football-club/ultimate-team");
+        URI uri = new URI("https://www.easports.com/fifa/ultimate-team/web-app");
+
         HttpContext hc = new BasicHttpContext();
-        //System.out.println(hg.getURI());
-        CloseableHttpResponse response = httpClient.execute(hg,hc);
-        if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK)
-            throw new IOException(response.getStatusLine().toString());
 
-        HttpUriRequest currentReq = (HttpUriRequest)hc.getAttribute(ExecutionContext.HTTP_REQUEST);
-        HttpHost currentHost = (HttpHost)  hc.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+        HttpGet hg = new HttpGet(uri);
+        HttpResponse response = httpClient.execute(hg,hc);
+        //uri.getHost()
+
+        HttpUriRequest currentReq = (HttpUriRequest)hc.getAttribute(HttpCoreContext.HTTP_REQUEST);
+        HttpHost currentHost = (HttpHost)  hc.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
 
         String currentUrl = (currentReq.getURI().isAbsolute()) ? currentReq.getURI().toString() : (currentHost.toURI() + currentReq.getURI());
         System.out.println(currentUrl);
 
+        String email = "leonwu127@icloud.com";
+        String password = "13655197351Wzq";
+        String secretAnswer = "weiwei";
+        Core core = new Core(email, password, secretAnswer);
+        String url = core.getRedirectUrl(email, password, secretAnswer, currentUrl, httpClient);
+
+        //String email = "17fifa.com@gmail.com";
+        //String password = "17FIFAcom";
+
+
+        //String url = "https://signin.ea.com/p/web/login?execution=e1857465993s2&initref=https%3A%2F%2Faccounts.ea.com%3A443%2Fconnect%2Fauth%3Fscope%3Dbasic.identity%2Bbasic.persona%2Bsignin%2Boffline%2Bsecurity.challenge%26redirect_uri%3Dhttp%253A%252F%252Fwww.easports.com%252Ffifa%252Flogin_check%26locale%3Den_GB%26state%3DeaD2c456XsUoTJR745VdMj_QcGizYTsUaEcY9IXSuKA%26response_type%3Dcode%26client_id%3DEASFC-web";
+        //System.out.println(url);
+        //String code = "000000";
+        //core.getWebApp(url,httpClient,code);
+        //HttpResponse loginResponse2 = httpClient.execute(hp2);
+        //getStringFromInputStream(loginResponse2.getEntity().getContent());
+
+    }
+
+    /**
+     *
+     * @param email user's email.
+     * @param password user's password.
+     * @param secretAnswer user's secretAnswer.
+     * @param currentUrl currentUrl.
+     * @param httpClient
+     * @return redirect url
+     * @throws IOException
+     */
+    public String getRedirectUrl(String email, String password,String secretAnswer, String currentUrl, CloseableHttpClient httpClient) throws IOException {
+        String redirectUrl = "";
         ArrayList<NameValuePair> nvp = new ArrayList<>();
 
-        String code = "495348";
-
+        // Prepare data.
         nvp.add(new BasicNameValuePair("_eventId","submit"));
+        nvp.add(new BasicNameValuePair("country","US"));
         nvp.add(new BasicNameValuePair("rememberMe","on"));
-        //nvp.add(new BasicNameValuePair("email","17fifa.com@gmail.com"));
-        nvp.add(new BasicNameValuePair("email","leonwu127@gmail.com"));
-        //nvp.add(new BasicNameValuePair("password","17FIFAcom"));
-        nvp.add(new BasicNameValuePair("password","2013Zhuangyuan"));
+        nvp.add(new BasicNameValuePair("email",email));
+        nvp.add(new BasicNameValuePair("password",password));
+        nvp.add(new BasicNameValuePair("phoneNumber",""));
+        nvp.add(new BasicNameValuePair("passwordForPhone",""));
+        nvp.add(new BasicNameValuePair("gCaptchaResponse",""));
+        nvp.add(new BasicNameValuePair("isPhoneNumberLogin","false"));
+        nvp.add(new BasicNameValuePair("isIncompletePhone",""));
         nvp.add(new BasicNameValuePair("_rememberMe","on"));
 
+        // Construct a http post request.
         HttpPost hp = new HttpPost(currentUrl);
+        HttpContext hc = new BasicHttpContext();
         hp.setEntity(new UrlEncodedFormEntity(nvp));
+        HttpResponse hr = httpClient.execute(hp,hc);
 
+        if (hr.getStatusLine().toString().equals("HTTP/1.1 302 Found")){
+            redirectUrl = hr.getFirstHeader("Location").getValue();
+            redirectUrl = "https://signin.ea.com" + redirectUrl + "&_eventId=end";
+            System.out.println(redirectUrl);
+        }
+        HttpContext nhc = new BasicHttpContext();
+        HttpGet hg = new HttpGet(redirectUrl);
+        HttpResponse hr2 = httpClient.execute(hg,nhc);
 
-        hp.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.122 Safari/537.36");
-        hp.addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        return redirectUrl;
+        //redirectUrl = m.group(1)+"&_eventId=end";
+        //System.out.println(m.group(1)+"&_eventId=end");
+
+        //HttpGet hg = new HttpGet(redirectUrl);
+        //HttpResponse hr2 = httpClient.execute(hg,nhc);
+
+        //hp.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.122 Safari/537.36");
+        //hp.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        /*
         hp.addHeader("Accept-Encoding","gzip,deflate,sdch");
         hp.addHeader("Accept-Language","en-US,en;q=0.8");
         hp.addHeader("Connection","keep-alive");
         hp.addHeader("DNT","1");
         hp.addHeader("Referer",currentUrl);
 
-
+        hp.setEntity(new UrlEncodedFormEntity(nvp));
         HttpContext hcResponse = new BasicHttpContext();
-
         HttpResponse loginResponse = httpClient.execute(hp,hcResponse);
         // Code get, now try to find the new url
 
+        // url redirection
+        String url = loginResponse.getHeaders("Location")[0].getValue().toString();
+        */
+        //HttpPost hp2 = new HttpPost(url);
+        //hp2.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.122 Safari/537.36");
+        //hp2.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        //hp2.addHeader("Accept-Encoding","gzip,deflate,sdch");
+        //hp2.addHeader("Accept-Language","en-US,en;q=0.8");
+        //hp2.addHeader("Connection","keep-alive");
+        //hp2.addHeader("DNT","1");
+        //hp2.addHeader("Referer",currentUrl);
+
+        //hp2.setEntity(new UrlEncodedFormEntity(nvp));
+        //HttpContext hcResponse2 = new BasicHttpContext();
+        //HttpResponse loginResponse2 = httpClient.execute(hp,hcResponse2);
+    }
 
 
-        String newUrl = loginResponse.getHeaders("Location")[0].getValue().toString();
-        //String newUrl = "https://signin.ea.com/p/web/login?execution=e1857465993s2&initref=https%3A%2F%2Faccounts.ea.com%3A443%2Fconnect%2Fauth%3Fscope%3Dbasic.identity%2Bbasic.persona%2Bsignin%2Boffline%2Bsecurity.challenge%26redirect_uri%3Dhttp%253A%252F%252Fwww.easports.com%252Ffifa%252Flogin_check%26locale%3Den_GB%26state%3DeaD2c456XsUoTJR745VdMj_QcGizYTsUaEcY9IXSuKA%26response_type%3Dcode%26client_id%3DEASFC-web";
-        System.out.println(newUrl);
 
+    public String getWebAppUrl(String url, CloseableHttpClient httpClient ,String code) throws IOException {
         ArrayList<NameValuePair> nvp2 = new ArrayList<>();
         nvp2.add(new BasicNameValuePair("_trustThisDevice","on"));
         nvp2.add(new BasicNameValuePair("trustThisDevice","on"));
-        nvp2.add(new BasicNameValuePair("twofactorCode","183229"));
+        nvp2.add(new BasicNameValuePair("twofactorCode",code));
         nvp2.add(new BasicNameValuePair("_eventId","submit"));
 
 
+        HttpPost hp2 = new HttpPost(url);
+        hp2.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10;0. Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.122 Safari/537.36");
+        hp2.addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        hp2.addHeader("Accept-Encoding","gzip,deflate,sdch");
+        hp2.addHeader("Accept-Language","en-US,en;q=0.8");
+        hp2.addHeader("Connection","keep-alive");
+        hp2.addHeader("DNT","1");
+        hp2.setHeader("Referer",url);
+        hp2.setEntity(new UrlEncodedFormEntity(nvp2));
 
-        //HttpPost hp2 = new HttpPost(newUrl);
-        hp.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.122 Safari/537.36");
-        hp.addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-        hp.addHeader("Accept-Encoding","gzip,deflate,sdch");
-        hp.addHeader("Accept-Language","en-US,en;q=0.8");
-        hp.addHeader("Connection","keep-alive");
-        hp.addHeader("DNT","1");
-        hp.setHeader("Referer",newUrl);
+        HttpResponse loginResponse2 = httpClient.execute(hp2);
+        String redirectUrl = getStringFromInputStream(loginResponse2.getEntity().getContent());
 
-
-        //hp2.setEntity(new UrlEncodedFormEntity(nvp2));
-
-        //HttpResponse loginResponse2 = httpClient.execute(hp2);
-
-        //getStringFromInputStream(loginResponse2.getEntity().getContent());
-
+        return redirectUrl;
     }
+
 
     private static String getStringFromInputStream(InputStream is) {
 
